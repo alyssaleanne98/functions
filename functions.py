d633ef67da8e598c0fe65ab7aff98a6d62fedb9f
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-01-28 15:09:18 -0800

    getting started

---
+++ functions.py
@@ -0,0 +1,21 @@
+#CHALLENGES
+
+#Write a function named sum_tothat accepts a single integer, n, and returns the sum of the integers from 1 to n.
+
+def sum_tothat(n): #function named sum_tothat accepts n
+    return 
+
+    # look into range
+
+#Write a function named largest that takes a list of numbers as an argument and returns the largest number in that list.
+
+#lambda function
+def largest(nums):
+    return max(nums)
+print (largest([10, 4, 2, 231, 91, 54]) ) # returns 231
+
+#for loop and if statement
+
+#Write a function named occurancesthat takes two string arguments as input and counts the number of occurances of the second string inside the first string.
+
+# look into a method 
